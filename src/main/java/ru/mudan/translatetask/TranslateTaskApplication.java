77c6884ff71ee782baba2bbf5c9a962ad18c793c
mudan <myudanov223@gmail.com>
2024-08-03 14:16:45 +0300

    initial commit - added dependencies in maven

---
+++ src/main/java/ru/mudan/translatetask/TranslateTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.mudan.translatetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslateTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslateTaskApplication.class, args);
+	}
+
+}
