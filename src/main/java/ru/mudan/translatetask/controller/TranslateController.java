c17c7baa1f894a8e62130401ba49c4b419b408cb
mudan <myudanov223@gmail.com>
2024-08-03 16:36:20 +0300

    added concurrent for translating words in phrase in 10 threads

--- src/main/java/ru/mudan/translatetask/controller/TranslateController.java
+++ src/main/java/ru/mudan/translatetask/controller/TranslateController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.mudan.translatetask.service.TranslateService;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Controller
 @RequestMapping("/translate")
 public class TranslateController {
@@ -27,9 +31,24 @@ public class TranslateController {
     public String send(Model model,
                        @RequestParam("q")String q,
                        @RequestParam("source")String source,
-                       @RequestParam("target")String target) {
-        String response = translateService.result(q,source,target);
-        model.addAttribute("response",response);
+                       @RequestParam("target")String target) throws InterruptedException {
+        String[] wordsInPhrase = q.split(" ");
+        String[]translatedWords = new String[wordsInPhrase.length];
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        CountDownLatch latch = new CountDownLatch(wordsInPhrase.length);
+        for(int i=0;i<wordsInPhrase.length;i++){
+            int index = i;
+            executorService.execute(() -> {
+                    String a = translateService.result(wordsInPhrase[index], source, target);
+                    translatedWords[index]=a;
+                    System.out.println(Thread.currentThread().getName()+" "+wordsInPhrase[index]);
+                    latch.countDown();
+            });
+        }
+        latch.await();
+
+        String translatedPhrase = String.join(" ", translatedWords);
+        model.addAttribute("response",translatedPhrase);
         return "views/index";
     }
 }
