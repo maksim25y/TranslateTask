9672590a1750a23751bb6ca2b44b93ad7d625847
mudan <myudanov223@gmail.com>
2024-08-04 13:25:17 +0300

    added unit-tests for TranslateService

--- src/test/java/ru/mudan/translatetask/TranslateTaskApplicationTests.java
+++ src/test/java/ru/mudan/translatetask/TranslateTaskApplicationTests.java
@@ -1,13 +1,49 @@
 package ru.mudan.translatetask;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.mudan.translatetask.service.TranslateService;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TranslateTaskApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TranslateService translateService;
+    private String translatedText;
+
+    @Test
+    void testTranslateFromEnToFr() {
+        translatedText = translateService.result("Hello", "en", "fr");
+        assertEquals("Bonjour", translatedText);
+    }
+    @Test
+    void testTranslateFromRuToEn() {
+        translatedText = translateService.result("Привет", "ru", "en");
+        assertEquals("Hello", translatedText);
+    }
+    @Test
+    void testTranslateWithBadCountry() {
+        assertThrows(RuntimeException.class,() ->translateService.result("Привет", "ru", "some"));
+    }
+    @Test
+    void testLanguagesContainsRussia() {
+        assertTrue(translateService.containsLanguage("ru"));
+    }
+    @Test
+    void testLanguagesContainsSpain() {
+        assertTrue(translateService.containsLanguage("es"));
+    }
+    @Test
+    void testLanguagesContainsEngland() {
+        assertTrue(translateService.containsLanguage("en"));
+    }
+    @Test
+    void testLanguagesDoesNotContainsBadCountry() {
+        assertFalse(translateService.containsLanguage("bad"));
+    }
+
 
 }
